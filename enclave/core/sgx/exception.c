ccd970d54fccd73bb8111816644ddb5f485dcf32
Mike Brasher <mikbras@microsoft.com>
2019-10-14 18:19:41 -0500

    Add second-level exception continuation hook
    The current enclave exception handler has a stack format that does not
    allow for ocalls (rsp does not point to the correct stack frame). Add
    a second-level exception handler that is called after stack state is
    restored to allow the handler to make ocalls.
    
    Additionally, add a test which makes use of this functionality.
    
    Signed-off-by: Mike Brasher <mikbras@microsoft.com>
    Co-authored-by: Jordan Hand <jorhand@microsoft.com>
    Co-authored-by: Mike Brasher <mikbras@microsoft.com>

--- enclave/core/sgx/exception.c
+++ enclave/core/sgx/exception.c
@@ -226,6 +226,8 @@ int _emulate_illegal_instruction(sgx_ssa_gpr_t* ssa_gpr)
     return -1;
 }
 
+void (*oe_continue_execution_hook)(oe_exception_record_t* oe_exception_record);
+
 /*
 **==============================================================================
 **
@@ -278,6 +280,10 @@ void oe_real_exception_dispatcher(oe_context_t* oe_context)
         // handling.
         td->host_rbp = td->host_previous_rbp;
         td->host_rsp = td->host_previous_rsp;
+
+        /* Call the user's continue-execution handler. */
+        if (oe_continue_execution_hook)
+            (*oe_continue_execution_hook)(&oe_exception_record);
 
         oe_continue_execution(oe_exception_record.context);
 
